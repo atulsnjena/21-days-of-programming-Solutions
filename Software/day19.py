6b44b46ad9d78109d01e7e73872163fde67292a1
Surya Narayan Jena <30215820+atulsnjena@users.noreply.github.com>
2020-07-05 20:40:43 +0530

    Create day19.py

---
+++ Software/day19.py
@@ -0,0 +1,18 @@
+a=int(input())
+b=int(input())
+n=int(input())
+o=0
+for i in range(a,b):
+    if i**2 <= b:
+        o=o+1
+p=o/(b-a+1)
+def nCr(n, r): 
+    return (fact(n) / (fact(r) * fact(n - r))) 
+  
+def fact(n): 
+    f = 1
+    for i in range(2, n+1): 
+        f = f * i 
+    return f
+P=nCr(10,1)*(p**1)*((1-p)**(10-1))
+print(P)
