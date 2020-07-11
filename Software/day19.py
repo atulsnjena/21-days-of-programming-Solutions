7e7e7550fb04fd31aa71031a612d125371509923
Surya Narayan Jena <30215820+atulsnjena@users.noreply.github.com>
2020-07-11 10:16:55 +0530

    Update day19.py

--- Software/day19.py
+++ Software/day19.py
@@ -1,18 +1,14 @@
+import random
 a=int(input())
 b=int(input())
 n=int(input())
 o=0
-for i in range(a,b):
-    if i**2 <= b:
-        o=o+1
-p=o/(b-a+1)
-def nCr(n, r): 
-    return (fact(n) / (fact(r) * fact(n - r))) 
-  
-def fact(n): 
-    f = 1
-    for i in range(2, n+1): 
-        f = f * i 
-    return f
-P=nCr(n,1)*(p**1)*((1-p)**(n-1))
-print(P)
+for j in range(n):
+    k=random.randint(a,b)
+    print(k,end=' ')
+    for i in range(a,k+1):
+        if i**2==k:
+            o=o+1
+print("")
+p=o/n
+print("Perfect Squares Observed=%d\nProbability=%f"%(o,p))
